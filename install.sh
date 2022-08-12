af428c42d2b472c99fa2c58f90ab5c39e689e0f3
Root-didi <73555229+Root-didi@users.noreply.github.com>
2022-08-12 22:22:28 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,9 +29,9 @@ set -e
 #############################################################################
 
 SCRIPT_VERSION="v0.11.0"
-GITHUB_BASE_URL="https://raw.githubusercontent.com/vilhelmprytz/pterodactyl-installer"
+GITHUB_BASE_URL="https://raw.githubusercontent.com/-didi/pterodactyl-installer"
 
-LOG_PATH="/var/log/pterodactyl-installer.log"
+LOG_PATH="/var/log/jexactyl-installer.log"
 
 # exit with error status code if user is not root
 if [[ $EUID -ne 0 ]]; then
